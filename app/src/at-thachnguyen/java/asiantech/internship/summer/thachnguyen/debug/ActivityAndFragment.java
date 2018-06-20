dfb1398011a5d4cdfa8aae185dda99be7178e63f
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-20 10:43:46 +0700

    create ActivityAndFragment

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/ActivityAndFragment.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/ActivityAndFragment.java
@@ -3,6 +3,8 @@ package asiantech.internship.summer.thachnguyen.debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import asiantech.internship.summer.R;
+
 public class ActivityAndFragment extends AppCompatActivity {
 
     @Override
