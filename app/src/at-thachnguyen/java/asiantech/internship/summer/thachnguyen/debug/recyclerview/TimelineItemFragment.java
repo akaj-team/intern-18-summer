0668b17b8e4324a3f31116061f57643c2f7c4555
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-27 19:06:05 +0700

    add progessbar

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/TimelineItemFragment.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/TimelineItemFragment.java
@@ -9,8 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
+
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.thachnguyen.debug.recyclerview.model.TimelineItem;
 
@@ -18,9 +20,13 @@ import static asiantech.internship.summer.R.layout.fragment_timeline_item;
 
 
 public class TimelineItemFragment extends Fragment {
+    private ProgressBar mProgressBarLoad;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(fragment_timeline_item, container, false);
+        mProgressBarLoad = view.findViewById(R.id.progressBarLoad);
         return inflater.inflate(fragment_timeline_item, container, false);
     }
 
@@ -28,10 +34,12 @@ public class TimelineItemFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ArrayList<TimelineItem> mListTimelines = TimelineItem.creatListTimeline();
-        RecyclerView mRecyclerViewTimeline = view.findViewById(R.id.recyclerViewTimeline);
-        TimelineAdapter timelineAdapter=new TimelineAdapter(getContext(), mListTimelines);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
-        mRecyclerViewTimeline.setAdapter(timelineAdapter);
-        mRecyclerViewTimeline.setLayoutManager(layoutManager);
+        RecyclerView recyclerViewTimeline = view.findViewById(R.id.recyclerViewTimeline);
+        TimelineAdapter timelineAdapter = new TimelineAdapter(getContext(), mListTimelines);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewTimeline.setAdapter(timelineAdapter);
+        recyclerViewTimeline.setLayoutManager(layoutManager);
+        mProgressBarLoad.setVisibility(View.VISIBLE);
+        mProgressBarLoad.setVisibility(View.GONE);
     }
 }
