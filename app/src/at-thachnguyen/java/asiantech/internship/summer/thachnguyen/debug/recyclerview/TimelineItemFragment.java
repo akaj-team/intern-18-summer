3ada880484c0fb9e8f525d987cd0ddd8764f902c
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-29 09:31:40 +0700

    fix convention

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/TimelineItemFragment.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/TimelineItemFragment.java
@@ -81,7 +81,7 @@ public class TimelineItemFragment extends Fragment {
         });
     }
 
-    private void CreateListTimeLine() {
+    private void createListTimeLine() {
         for (int i = 0; i < 10; i++) {
             Owner owner = new Owner(RecyclerViewActivity.getName(i % 5), RecyclerViewActivity.getAvatar(i % 5));
             mTimelines.add(new TimelineItem(owner, RecyclerViewActivity.randomImageFood("food", 22), RecyclerViewActivity.getDescription(i), 0));
@@ -92,14 +92,14 @@ public class TimelineItemFragment extends Fragment {
     private void loadMoreTimeLine() {
         mProgressBarLoad.setVisibility(View.VISIBLE);
         new Handler().postDelayed(() -> {
-            CreateListTimeLine();
+            createListTimeLine();
             mProgressBarLoad.setVisibility(View.GONE);
         }, 5000);
     }
 
     private void loadRefreshTimeLine() {
         mTimelines.clear();
-        CreateListTimeLine();
+        createListTimeLine();
         new Handler().postDelayed(() ->
                         mSwipeRefreshLayout.setRefreshing(false),
                 2000);
