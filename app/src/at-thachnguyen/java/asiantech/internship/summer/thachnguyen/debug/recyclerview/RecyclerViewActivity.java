80c10c11f1bbea58d28b0168978cfbd5465fa167
Thach Nguyen H <thach.nguyen2@asiantech.vn>
2018-06-29 09:03:53 +0700

    add toolbar

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/RecyclerViewActivity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/recyclerview/RecyclerViewActivity.java
@@ -6,7 +6,9 @@ import android.content.res.Resources;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+import java.util.Objects;
 import java.util.Random;
 
 import asiantech.internship.summer.R;
@@ -24,6 +26,12 @@ public class RecyclerViewActivity extends AppCompatActivity {
         mPackagename = getPackageName();
         mResources = getResources();
         mContext = getApplicationContext();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Time Line");
+
         TimelineItemFragment timelineItemFragment = new TimelineItemFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragmentContainer, timelineItemFragment);
