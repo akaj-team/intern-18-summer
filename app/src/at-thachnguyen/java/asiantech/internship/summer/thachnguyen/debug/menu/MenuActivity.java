6bcbeb65832e50c6af514d333ebaa33a53c40b39
Thach Nguyen H <40165885+at-thachnguyen2@users.noreply.github.com>
2018-06-29 09:14:44 +0700

    Update MenuActivity.java

--- app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/menu/MenuActivity.java
+++ app/src/at-thachnguyen/java/asiantech/internship/summer/thachnguyen/debug/menu/MenuActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.thachnguyen.debug.activity_and_fragment.ActivityAndFragment;
 import asiantech.internship.summer.thachnguyen.debug.recyclerview.RecyclerViewActivity;
