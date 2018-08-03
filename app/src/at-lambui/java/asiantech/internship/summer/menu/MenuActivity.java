4c2ae0e2f84f9bae7dfedb0919e6bd6b321f3315
at-lambui <lam.bui@asiantech.vn>
2018-08-03 16:59:28 +0700

    fix CI

--- app/src/at-lambui/java/asiantech/internship/summer/menu/MenuActivity.java
+++ app/src/at-lambui/java/asiantech/internship/summer/menu/MenuActivity.java
@@ -3,6 +3,7 @@ package asiantech.internship.summer.menu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import asiantech.internship.summer.R;
@@ -13,37 +14,58 @@ import asiantech.internship.summer.ViewAndViewGruopActivity;
 import asiantech.internship.summer.filestorage.FileStorageActivity;
 import asiantech.internship.summer.viewpager.PagerActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mBtnViewAndViewGroup;
+    Button mBtnFragmentAndListener;
+    Button mBtnRecyclerView;
+    Button mBtnViewPager;
+    Button mBtnFileStorage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Button btnRecyclerview = findViewById(R.id.btnRecyclerview);
+        initViews();
+        addListeners();
+    }
+    private void initViews() {
+        mBtnViewAndViewGroup = findViewById(R.id.btnViewAndViewGroup);
+        mBtnFragmentAndListener = findViewById(R.id.btnExerciseFragment);
+        mBtnRecyclerView = findViewById(R.id.btnRecyclerview);
+        mBtnViewPager = findViewById(R.id.btnViewPager);
+        mBtnFileStorage = findViewById(R.id.btnFileStorage);
 
-        Button btn_ExerciseFragment = findViewById(R.id.btnExerciseFragment);
-        btn_ExerciseFragment.setOnClickListener(v -> {
-            Intent intentFragment = new Intent(MenuActivity.this, HomeActivity.class);
-            startActivity(intentFragment);
-        });
-        btnRecyclerview.setOnClickListener(view -> {
-            Intent intentRecyclerview = new Intent(MenuActivity.this, RecyclerViewActivity.class);
-            startActivity(intentRecyclerview);
-        });
-        Button btnViewAndViewGroup = findViewById(R.id.btnViewAndViewGroup);
-        btnViewAndViewGroup.setOnClickListener(view -> {
-            Intent intent = new Intent(MenuActivity.this, ViewAndViewGruopActivity.class);
-            startActivity(intent);
-        });
-        Button btnViewpager = findViewById(R.id.btnViewPager);
-        btnViewpager.setOnClickListener(view -> {
-            Intent intentViewpager = new Intent(MenuActivity.this, PagerActivity.class);
-            startActivity(intentViewpager);
-        });
-        Button btnFileStorage = findViewById(R.id.btnFileStorage);
-        btnFileStorage.setOnClickListener(view -> {
-            Intent intentFileStorage = new Intent(MenuActivity.this, FileStorageActivity.class);
-            startActivity(intentFileStorage);
-        });
-
+    }
+    private void addListeners() {
+        mBtnViewAndViewGroup.setOnClickListener(this);
+        mBtnFragmentAndListener.setOnClickListener(this);
+        mBtnRecyclerView.setOnClickListener(this);
+        mBtnViewPager.setOnClickListener(this);
+        mBtnFileStorage.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btnViewAndViewGroup:
+                intent = new Intent(MenuActivity.this, ViewAndViewGruopActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnExerciseFragment:
+                intent = new Intent(MenuActivity.this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnRecyclerview:
+                intent = new Intent(MenuActivity.this, RecyclerViewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnViewPager:
+                intent = new Intent(MenuActivity.this, PagerActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btnFileStorage:
+                intent = new Intent(MenuActivity.this, FileStorageActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
